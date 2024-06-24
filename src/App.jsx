e51e5bfae25e94857776e1bb60ee422bcd96168a
yeman2001 <127174129+yeman2001@users.noreply.github.com>
2024-06-24 09:27:40 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Hello, i'm Yeman</p>
+      <p className="read-the-docs">Hello, i am Yeman</p>
     </>
   );
 }
